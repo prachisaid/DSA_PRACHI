8c0da280553f4d381dba526a679abdcf762d37a9
Prachi Said <prachisaid16@gmail.com>
2024-12-08 22:22:53 +0530

    dsa

---
+++ src/POTD/TwoBestNonOverlappingEvents.java
@@ -0,0 +1,43 @@
+package POTD;
+
+public class TwoBestNonOverlappingEvents {
+	public static void main(String[] args) {
+
+	}
+
+	public int maxTwoEvents(int[][] events) {
+		long[] start = new long[events.length];
+		long[] end = new long[events.length];
+
+		for(int i = 0; i < events.length; i++) {
+			long startValue = ((long) events[i][0] << 32) | (0xFFFF_FFFFL & i);
+			long endValue = ((long) (events[i][1]+1) << 32) | (0xFFFF_FFFFL & i);
+			start[i] = startValue;
+			end[i] = endValue;
+		}
+
+		Arrays.sort(start);
+		Arrays.sort(end);
+
+		int last = 0, sum = 0;
+
+		for(int i = 0, j = 0; i < events.length && j < events.length;) {
+			int startTime = (int)((start[i] >> 32) & 0xFFFF_FFFFL);
+			int startIdx = (int)(start[i] & 0xFFFF_FFFFL);
+			int endTime = (int)((end[j] >> 32) & 0xFFFF_FFFFL);
+			int endIdx = (int)(end[j] & 0xFFFF_FFFFL);
+
+			if(startTime < endTime) {
+				sum = Math.max(sum, last + events[startIdx][2]);
+				++i;
+			}
+			else {
+				last = Math.max(last, events[endIdx][2]);
+				++j;
+			}
+		}
+
+		return sum;
+
+	}
+}
